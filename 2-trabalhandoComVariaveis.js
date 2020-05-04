5303461e9a0d59836e839ffd79983b9862b6e5c9
Jorge Ferraz <jferrazjorge@gamil.com>
2020-05-04 12:25:24 -0300

    Aula 2 - Tipos de Variáveis

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com Variáveis");
+
+const idade = 35;
+
+const nome = "Jorge";
+
+console.log("Nome : "+ nome);
+console.log("Idade = " +idade);
---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de Variáveis");
+
+const idade = 35;
+
+let primeiroNome = "Jorge";
+
+const sobrenome = " Ferraz";
+
+//console.log("Nome : "+ nome + " "+ sobrenome);
+console.log("Nome: " +primeiroNome, sobrenome)
+console.log("Idade = " +idade);
+
+console.log(`Meu nome é ${primeiroNome} com idade de ${idade} anos` );
+
+nomeCompleto = primeiroNome + sobrenome;
+
+console.log(`Meu nome é ${nomeCompleto} com idade de ${idade} anos` );
