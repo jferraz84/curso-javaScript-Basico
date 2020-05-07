219a7530d3ecf6b82f588e5851ddf4a933d1916c
Jorge Ferraz <jferrazjorge@gamil.com>
2020-05-07 11:21:42 -0300

    Módulo 2 POO - Aula 1 - Conhecendo o problema do cliente

--- poo-JS/ContaCorrente.js
+++ poo-JS/ContaCorrente.js
@@ -3,9 +3,6 @@ import { Cliente } from "./Cliente.js";
 export class ContaCorrente {
     
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
-    _saldo = 0;
 
     set cliente(novoValor){
         if (novoValor instanceof Cliente) {
@@ -25,6 +22,8 @@ export class ContaCorrente {
     constructor(agencia, cliente){
         this.agencia =  agencia;
         this.cliente = cliente;
+        this._saldo = 0;
+
         ContaCorrente.numeroDeContas += 1;
 
     }
--- poo-JS/index.js
+++ poo-JS/index.js
@@ -1,21 +1,17 @@
 import{Cliente} from "./Cliente.js";
 import{ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import{Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Jorge", 11122233309);   
-const cliente2 = new Cliente("Aline", 44455566609);
 
-const contaCorrenteRicardo = new ContaCorrente(1001,cliente1);
-contaCorrenteRicardo.depositar(500);
+const contaCorrenteJorge = new Conta(0, cliente1, 1001);
+contaCorrenteJorge.depositar(500);
+contaCorrenteJorge.sacar(100);
 
-const conta2 = new ContaCorrente(102,cliente2 );
-let valor = 200;
-contaCorrenteRicardo.transferir(valor, conta2);
+const contaPoupanca = new Conta(150, cliente1, 1001);
 
-console.log("Numero de Contas no Banco: " +ContaCorrente.numeroDeContas);
-console.log();
-console.log(contaCorrenteRicardo);
-console.log(conta2);
-
-
+console.log(contaPoupanca);
+console.log(contaCorrenteJorge);
 
 
