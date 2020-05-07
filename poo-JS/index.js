49999216ef6a3f68cee5c2beca8340860a92b5b5
Jorge Ferraz <jferrazjorge@gamil.com>
2020-05-07 12:27:29 -0300

    Aula 2 - Herança

--- poo-JS/index.js
+++ poo-JS/index.js
@@ -1,15 +1,15 @@
 import{Cliente} from "./Cliente.js";
 import{ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import{Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Jorge", 11122233309);   
 
-const contaCorrenteJorge = new Conta(0, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(150, cliente1, 1001);
+
+const contaCorrenteJorge = new ContaCorrente(cliente1, 1001);
 contaCorrenteJorge.depositar(500);
 contaCorrenteJorge.sacar(100);
 
-const contaPoupanca = new Conta(150, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteJorge);
